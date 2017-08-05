1133add94edf6b60bb998544d4f8a11a26e11ba6
zhaohaifeng <zhaoImpulse@gmail.com>
2017-08-05 14:33:03 +0800

    引用vue作为前端框架

--- server/app.js
+++ server/app.js
@@ -5,14 +5,14 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
-var mosca = require('./mqttServer');
+var index = require('./src/routes/index');
+var users = require('./src/routes/users');
+// var mosca = require('./src/mqttServer');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'src/views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
@@ -21,7 +21,11 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+  "/",
+  express.static(__dirname)
+);
+// app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var path = require('path');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  console.log(__dirname)
+  res.sendFile(path.join(__dirname,'index.html'))
+  return ;
+  // res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
